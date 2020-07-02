702fc2c110cfd3e8829f2d9d3fd1816d09473cdd
Farhan Ahmed <farhan1086@gmail.com>
2020-07-02 11:51:21 +0530

    initial commit

---
+++ ArrayCopyMethodAndArrayCloneMethod_Demo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ArrayCopyMethodAndArrayCloneMethod_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = new int[10] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+            foreach (var item in Numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Using Copy to Method ");
+
+            int[] Number2 = new int[10];
+            Numbers.CopyTo(Number2, 0);
+            foreach (var item in Number2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Using Clone to Method ");
+            int[] Number3 = (int[])Numbers.Clone();
+            foreach (var item in Number3)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
